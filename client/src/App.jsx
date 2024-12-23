b55d20bfc4ef9042ac58ae5045384cf1552ffb7f
aliraza3180 <aliraza.dev08@gmail.com>
2024-12-23 21:06:59 +0500

    NavBar Compeleted

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route , Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import ApplyJob from './pages/ApplyJob.jsx'
+import Applications from './pages/Applications.jsx'
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/apply-job/:id' element={<ApplyJob />}/>
+        <Route path='/applications' element={<Applications />}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
